8f67e93d297dbf5dfe3d14f823b60c4a2fac00b0
AbdullahAkman <93098664+AbdullahAkman@users.noreply.github.com>
2021-11-25 23:23:30 +0300

    Add files via upload

---
+++ day20_scope/StaticVariables.java
@@ -0,0 +1,23 @@
+package day20_scope;
+
+public class StaticVariables {
+
+	static int no=20;
+	static int sayi;
+	static String adres="Ankara";
+	static String cadde;
+	static boolean aktifMi=true;
+	static boolean tatildeMi;
+	
+	
+	
+	public static void main(String[] args) {
+		
+	    
+		System.out.println(no); // 20
+		no++;
+		System.out.println(no);
+
+	}
+
+}
