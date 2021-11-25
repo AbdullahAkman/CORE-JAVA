3b0062eef93ed9d1f3701fb2e2b1e064b4dee0f7
AbdullahAkman <93098664+AbdullahAkman@users.noreply.github.com>
2021-11-25 23:19:55 +0300

    Add files via upload

---
+++ day10_ternary_SwitchCase/C08_SwitchCase.java
@@ -0,0 +1,13 @@
+package day10_ternary_SwitchCase;
+public class C08_SwitchCase {
+    public static void main(String[] args) {
+        //gun numarasina gore hafta ici veya hafta sonu yazdiralim
+        
+        
+    	
+    	
+    	
+    	
+    	
+    }
+}
