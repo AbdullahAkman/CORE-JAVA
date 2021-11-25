03672badaae85bcbc24366aacabb9f788349cad7
AbdullahAkman <93098664+AbdullahAkman@users.noreply.github.com>
2021-11-25 23:12:35 +0300

    Add files via upload

---
+++ day02_Odevler/C06_Odevler.java
@@ -0,0 +1,38 @@
+package day02_Odevler;
+
+import java.util.Scanner;
+
+public class C06_Odevler {
+
+	public static void main(String[] args) {
+
+		
+		// Kullanicidan iki tamsayý alip bu sayilarin toplam, 
+		// fark ve carpimlarini yazdirin.
+		
+	int A = 20;
+	int B = 10;
+	System.out.println(A+B);
+	
+	System.out.println(A-B);
+	
+	System.out.println(A*B);
+	
+	
+	
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Lutfen karenin bir kenarini cm olarak giriniz:");
+	
+	
+	
+			
+	int a = 10;
+	int b = 20;
+	
+	System.out.println(a+b);
+		
+	
+	}
+
+}
